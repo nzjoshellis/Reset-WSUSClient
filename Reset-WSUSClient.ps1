b5df0614fb36efde035f218d42cc01573c767254
Josh Ellis <Josh.Ellis@outlook.com>
2016-02-08 15:43:54 +1300

    Create Reset-WSUSClient.ps1

---
+++ Reset-WSUSClient.ps1
@@ -0,0 +1,27 @@
+################################################################
+# SCRIPT: Reset-WSUSClient.ps1
+# AUTHOR: Josh Ellis
+# VERSION: 1.0
+# DATE: 08/02/2016
+# DESCRIPTION: Resets the WSUS Client ID and clears our C:\Windows\SoftwareDistribution.
+################################################################
+
+$Path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate"
+
+#Stop Service
+Stop-Service "Windows Update"
+
+#Remove Reg Keys
+Get-ItemProperty -Path $Path | Remove-ItemProperty -Name 'PingID' -Force | Out-Null
+Get-ItemProperty -Path $Path | Remove-ItemProperty -Name 'AccountDomainSid' -Force | Out-Null
+Get-ItemProperty -Path $Path | Remove-ItemProperty -Name 'SusClientID' -Force | Out-Null
+Get-ItemProperty -Path $Path | Remove-ItemProperty -Name 'SusClientIdValidation' -Force | Out-Null
+
+#Remove SoftWare Distribution Folder
+Remove-Item "C:\Windows\SoftwareDistribution" -Recurse -Force
+
+#Start Service
+Start-Service "Windows Update"
+
+#Check into WSUS
+wuauclt.exe /resetauthorization /detectnow 
